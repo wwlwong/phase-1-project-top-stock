df9e313c9b0775899e10e0a2ab86863708d411b4
William Wong <william.wai.lim.wong@gmail.com>
2023-05-09 22:52:38 -0400

    adds db.json

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-fetch('https://tradestie.com/api/v1/apps/reddit')
-  .then(response => response.json())
-  .then(stocks => console.log(stocks));
+function fetchStocks(){
+    return fetch('https://tradestie.com/api/v1/apps/reddit')
+    .then(response => response.json())
+    .then(stocks => renderStocks(stocks));
+}
+
+function renderStocks(stocks) {
+    const output = document.querySelector('output');
+    stocks.forEach(stock => {
+      const tr = document.createElement('tr')
+      const td0 = document.createElement('td');
+      td0.innerHTML = stock.ticker;
+
+      const td1 = document.createElement('td');
+      td1.innerHTML = stock.sentiment;
+
+      const td2 = document.createElement('td');
+      td2.innerHTML = stock.sentiment_score;
+      output.append(tr, td0, td1, td2);
+    });
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchStocks();
+  });
