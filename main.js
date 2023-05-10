8ce2e33edacc7937288d232281e236bd71370dcc
William Wong <william.wai.lim.wong@gmail.com>
2023-05-10 11:07:14 -0400

    creates function to add table elements

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 function fetchStocks(){
     return fetch('http://localhost:3000/stocks')
     .then(response => response.json())
-    .then(stocks => renderStocks(stocks));
+    .then(stocks => renderAllStocks(stocks));
 }
 
-function renderStocks(stocks) {
-    const output = document.querySelector('.output');
-    stocks.forEach(stock => {
-      const tr = document.createElement('tr')
-      const td0 = document.createElement('td');
-      td0.innerHTML = stock.ticker;
+function renderAllStocks(stocks) {
+    const tableBody = document.getElementById('tableBody');
+    stocks.forEach(stock => addTableBody(stock));
+    //   {
+    //   const tr = document.createElement('tr')
+    //   const td0 = document.createElement('td');
+    //   td0.innerHTML = stock.ticker;
 
-      const td1 = document.createElement('td');
-      td1.innerHTML = stock.sentiment;
+    //   const td1 = document.createElement('td');
+    //   td1.innerHTML = stock.sentiment;
 
-      const td2 = document.createElement('td');
-      td2.innerHTML = stock.sentiment_score;
-      output.append(tr, td0, td1, td2);
-    });
+    //   const td2 = document.createElement('td');
+    //   td2.innerHTML = stock.sentiment_score;
+    //   tableBody.append(tr, td0, td1, td2);
+    // });
   }
+
+function addTableBody(stock){
+    const tr = document.createElement('tr')
+    const td0 = document.createElement('td');
+    td0.innerHTML = stock.ticker;
+
+    const td1 = document.createElement('td');
+    td1.innerHTML = stock.sentiment;
+
+    const td2 = document.createElement('td');
+    td2.innerHTML = stock.sentiment_score;
+    tableBody.append(tr, td0, td1, td2);
+}
+
+function removeTableBody() {
+  let element = document.getElementById("tableBody");
+ 
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }  
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchStocks();
